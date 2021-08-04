c896b0b3876addeb13fc9a7621e34c08fc38992d
dev <dev.aikosia@gmail.com>
2021-08-04 14:42:23 +0700

    update to 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,4 +4,9 @@
 
 ## 0.0.2
 
-1. change ColumnDefinition params fieldName to name
+1. change class ColumnDefinition params fieldName to name
+
+## 0.0.3
+
+1. change class ColumnDefinition to DbName
+2. change OdooModel params idFieldName to id
--- lib/src/annotation.dart
+++ lib/src/annotation.dart
@@ -1,20 +1,25 @@
-class ColumnDefinition {
+class DbColumn {
   final String name;
   final Type type;
   final bool required;
 
-  const ColumnDefinition(
+  const DbColumn(
       {required this.name, required this.type, this.required = false});
 }
 
 class OdooModel {
   final String tableName;
+
   final String className;
-  final List<ColumnDefinition> columns;
-  final String idFieldName;
+
+  final List<DbColumn> columns;
+
+  ///which column is id
+  final String id;
+
   const OdooModel(
       {required this.className,
       required this.tableName,
       required this.columns,
-      this.idFieldName = "id"});
+      this.id = "id"});
 }
